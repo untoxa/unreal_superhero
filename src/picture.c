27a74721c4d3ccdb4d6ca9abf7f9d48b5e9198da
Toxa <untoxa@mail.ru>
2021-07-12 14:33:31 +0300

    update to support the latest version of incbin.h

--- src/picture.c
+++ src/picture.c
@@ -1,12 +1,12 @@
 #include <gb/incbin.h>
 
-INCBIN(upper_picture_tiles, picture/unreal2_upper.til)
-INCBIN(upper_picture_map, picture/unreal2_upper.map)
-INCBIN(upper_picture_attr, picture/unreal2_upper.atr)
+INCBIN(upper_picture_tiles, "picture/unreal2_upper.til")
+INCBIN(upper_picture_map, "picture/unreal2_upper.map")
+INCBIN(upper_picture_attr, "picture/unreal2_upper.atr")
 
-INCBIN(lower_picture_tiles, picture/unreal2_lower.til)
-INCBIN(lower_picture_map, picture/unreal2_lower.map)
-INCBIN(lower_picture_attr, picture/unreal2_lower.atr)
+INCBIN(lower_picture_tiles, "picture/unreal2_lower.til")
+INCBIN(lower_picture_map, "picture/unreal2_lower.map")
+INCBIN(lower_picture_attr, "picture/unreal2_lower.atr")
 
 
-INCBIN(picture_palette, picture/unreal2.pal)
+INCBIN(picture_palette, "picture/unreal2.pal")
